4133801158f5e879b787c16bd6a268ce87762638
Rudra-1587 <agravatrudra159@gmail.com>
2026-07-04 18:31:13 +0530

    Created required files for project

---
+++ src/main/java/com/example/employee_management_API/DTO/EmployeeDTO.java
@@ -0,0 +1,18 @@
+package com.example.employee_management_API.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Double salary;
+
+}
