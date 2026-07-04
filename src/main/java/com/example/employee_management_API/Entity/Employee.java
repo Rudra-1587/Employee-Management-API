8e8fdfed689008ec261f6949816f3c9a88d0ea93
Rudra-1587 <agravatrudra159@gmail.com>
2026-07-04 18:36:08 +0530

    create Entity of database

--- src/main/java/com/example/employee_management_API/Entity/Employee.java
+++ src/main/java/com/example/employee_management_API/Entity/Employee.java
@@ -1,4 +1,28 @@
 package com.example.employee_management_API.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private Double salary;
+
+
 }
